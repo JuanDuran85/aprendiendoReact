0b622a4428abb8c6daeadbbbb87feadc5148d563
JuanDuran85 <duran85juan@gmail.com>
2019-12-13 22:51:22 -0300

    agregando y usando librarias de react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-const elemento = document.createElement('h1'); //se crea un elemento en el html
-elemento.innerText = "Programando desde React"; // agregando el mensaje al elemento en html
+// const elemento = document.createElement('h1'); //se crea un elemento en el html
+// elemento.innerText = "Programando sin React"; // agregando el mensaje al elemento en html
 
+// const contenedor = document.getElementById('root');
+
+// contenedor.appendChild(elemento);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const elemento = <h1>Programando con React!!!!!</h1>;
 const contenedor = document.getElementById('root');
 
-contenedor.appendChild(elemento);
+ReactDOM.render(elemento,contenedor);
