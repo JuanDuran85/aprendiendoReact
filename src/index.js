4f16e5cd2543ad5d03e9f01bf1bac2c706153e07
JuanDuran85 <duran85juan@gmail.com>
2019-12-13 23:36:18 -0300

    agregado un reloj al index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const elemento = <h1>Programando con React!!!!!</h1>;
-const contenedor = document.getElementById('root');
+const elemento = <h1>Programando con React!!!!!</h1>; //se crea el elemento
+const contenedor = document.getElementById('root'); //se define el dom donde se mostrara el elemento
 
-ReactDOM.render(elemento,contenedor);
+ReactDOM.render(elemento,contenedor); //renderizando 
+
+const nombre = <span>Juan Duran</span> // creando una variable cualquiera
+const numero = 3*6;
+const muestra = <h2>Tu nombre es: {nombre}. Ejemplo usando React. El numero es: {numero}</h2> //agregando la varianle a otra variable
+const contenedor2 = document.getElementById('segundo');
+
+ReactDOM.render(muestra,contenedor2);
+
+// creamos una variable con un estilo predeterminado
+const estilo = {
+    color: 'blue', backgroundColor: 'white'
+};
+
+//funcion que insertara la hora actual
+function hora() {
+  const elemento2 = (
+      <section>
+        <h2 style={estilo}>Reloj en React</h2>
+        <h3>Son las {new Date().toLocaleTimeString()}</h3>  
+      </section>
+  );
+  const contenedor3 = document.getElementById('hora');
+  ReactDOM.render(elemento2,contenedor3);
+};
+
+setInterval(hora,1000);
