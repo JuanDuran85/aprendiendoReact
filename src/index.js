cc5d7f86fd1be01d4a00c73e1ce7845a92f821ec
JuanDuran85 <duran85juan@gmail.com>
2019-12-13 22:39:03 -0300

    modificando el archivo index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+const elemento = document.createElement('h1'); //se crea un elemento en el html
+elemento.innerText = "Programando desde React"; // agregando el mensaje al elemento en html
 
+const contenedor = document.getElementById('root');
+
+contenedor.appendChild(elemento);
